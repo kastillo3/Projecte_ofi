73492e0c4ad7650277021231284d96dc6a293890
ToniIND <32127242+ToniIND@users.noreply.github.com>
2018-04-25 12:10:41 +0200

    Update projecte.py

--- projecte.py
+++ projecte.py
@@ -186,30 +186,41 @@ def muerto():
     return logo
 
 images = [base, fallo_1, fallo_2, cabeza, torso, bras_1, bras_2, cama_1, cama_2, muerto]
+#Dins de la llista "images" emmagatzem totes les funcions definides. 
 
-
-MAX_WRONG = len(images)
+MAX_WRONG = len(images) 
+#Emmagatzemem la llargada de la llista.
 print "Bienvenido al ahorcado. Buena suerte :)"
 WORDS = input("Escriba una palabra: ")
+#Demanem la paraula a encertar.
 LIST_WORDS=[]
 LIST_WORDS+=[WORDS]
+#Hem creat una llista de la paraula que ha introduit el usuari.
 word = random.choice(LIST_WORDS)
+#Elegim la paraula de la llista, com dins la llista nomes hi ha la paraula del usuari, sempre la agafara.
 so_far = "-" * len(word)
+#Canviem la longitud de la paraula per "-".
 wrong = 0
 used = []
+#Iniciem el contador d'errors i la llista de paraules utilitzades.
 
 while wrong < MAX_WRONG and so_far != word:
+#Quan el contador sigui menor de la cantitat de les fincions creades que hi han en la llista
   s.set_pixels(images[wrong % len(images)]())
+#Amb això mostre els pixels que hem definit a la funcio  
   print "Has usado las siguientes letras", used
+#Imprimeix la llista que conte les lletres que has fet servir
   print "De momento, la palabra es: ", so_far
+#Sustitueix la longitud de la paraula per guions.
   guess = input("Introduzca una letra: ")
-
+#Et demana que introdueixis una lletra
   while guess in used:
     print "Ya habia introducido esta letra", guess
     guess = input("Introduzca una letra")
+#Mentres la lletra estigui a la llista used,et diu que ja has introduit la lletra i et demana que introdueixis una altre
 
   used.append(guess)
-  
+#Afegeix la lletra de la paraula a la llista used
   if guess in word:
     print "Si", guess, "esta en la palabra"
     new = ""
